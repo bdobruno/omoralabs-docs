aed19e07c4690574b35b4cf4e7d50cede78e29f0
Bruno de Oliveira <badeoliveirag@gmail.com>
2026-01-25 11:08:08 -0300

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import { RootProvider } from "fumadocs-ui/provider/next";
 import { Toaster } from "sonner";
 import "./global.css";
@@ -12,6 +13,7 @@ export default function Layout({ children }: LayoutProps<"/">) {
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
