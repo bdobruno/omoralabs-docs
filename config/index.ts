b3c1b24d4a2cea7ae9fa44f39e787700450b5823
Bruno de Oliveira <badeoliveirag@gmail.com>
2026-01-25 12:38:17 -0300

    adjusted metadata

--- config/index.ts
+++ config/index.ts
@@ -2,7 +2,7 @@ const config = {
   websiteName:
     "Building blocks for composable, modern finance analytics | Omora Docs",
   websiteUrl:
-    process.env.NEXT_PUBLIC_WEBSITE_URL || "https://docs.omoralabs.com/",
+    process.env.NEXT_PUBLIC_WEBSITE_URL || "https://docs.omoralabs.com",
   websiteDescription:
     "Building blocks for composable, modern finance analytics.",
 };
